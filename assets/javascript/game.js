c9cd1839d4714acfc63acadafc1146b80124f33a
Matthew Tutak <mtutak91@gmail.com>
2016-10-20 19:27:13 -0500

    starting logic of game with notes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,48 @@
 console.log("Space Jam")
+
+				var letterSelected = 
+                var wins = 0
+                var guessesRemaining = 20
+                var lettersGuessed = 0
+function selectPhrase (){
+	for (var i = 0; i >= phrases.length; i++) {
+                   console.log(arrayName[i]);
+                }
+}
+                //get in correct scope
+                var currentWord = phrases[i]
+                console.log(phrases[i]);
+//onkeyup property returns the onKeyUp event handler code for the current element
+document.onkeyup = function(event) {
+                //String.fromCharCode() returns a string from specified unicode values provided by event.KeyCode
+                // event.keyCode returns the Unicode character code of the key that triggered the onkeyup event
+                //.toLowerCase() converts said string to lower case
+                var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+                //select random word from array
+                //count number of characters in string from array
+                //innerHTML 'print' _ equivalent to number of strings in array
+               
+                // This sets the computer guess equal to the random.
+                //Math.floor object method is a function that returns the largest integer less than or equal to a given number
+                //Math.random  random method of Math object is a function number in the range [0, 1) that is, from 0 (inclusive) up to but not including 1 (exclusive), which you can then scale to your desired range.
+                var computerChoices = ['r', 'p', 's'];
+                var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+                //check if entering a keyboard character else prompt chose alphabetically character
+                //check if  userGuess === any of the string characters in the phrase
+ 
+                var phrases = ['Like_Mike', 'Special_Stuff', 'Daffy_Duck']
+                var arrayName = ['index 0', 'index 1', 'index 2' ];
+                arrayName.length
+                var name = "value"
+                if (true) {
+ 
+                                condition;
+                }else if() {
+ 
+                }else {
+ 
+                }
+ 
+                for (var i = 0; i >= arrayName.length; i++) {
+                                arrayName[i]
+                }
